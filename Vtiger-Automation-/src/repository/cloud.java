8846e09bede9841248f42c1d5a827b076b0df779
firstUser <firstUser@gmail.com>
2023-02-11 11:11:15 +0530

    vtiger

---
+++ Vtiger-Automation-/src/repository/cloud.java
@@ -0,0 +1,14 @@
+package repository;
+
+public class cloud {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+	} 
+	public static void m1() {
+		System.out.println("friday");
+		System.out.println("blue sky");
+		System.out.println("hello");
+	}
+}
